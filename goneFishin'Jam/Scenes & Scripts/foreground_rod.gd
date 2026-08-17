53802c873daa9efdcaeb9226736f3eeda01a278e
indecisivemagician <163153852+Indecisivv@users.noreply.github.com>
2026-08-16 23:44:39 -0400

    main menu and phone
    line boils, gui, all that good stuff

---
+++ goneFishin'Jam/Scenes & Scripts/foreground_rod.gd
@@ -0,0 +1,11 @@
+extends Sprite2D
+
+func _process(delta: float) -> void:
+	var visible_rect: Rect2 = get_viewport_rect()
+	var target_pos: Vector2 = get_global_mouse_position()
+	
+	# Clamp mouse coordinates inside the viewport bounds
+	target_pos.x = clamp(target_pos.x, visible_rect.position.x, visible_rect.end.x)
+	target_pos.y = clamp(target_pos.y, visible_rect.position.y, visible_rect.end.y)
+	
+	position += (target_pos *6 * delta) - position
