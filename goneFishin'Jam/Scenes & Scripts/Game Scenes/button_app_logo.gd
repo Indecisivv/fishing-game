eb6a8bae01d325848f4f4b748b269724ad942328
indecisivemagician <163153852+Indecisivv@users.noreply.github.com>
2026-08-19 22:25:22 -0400

    audio files

--- goneFishin'Jam/Scenes & Scripts/Game Scenes/button_app_logo.gd
+++ goneFishin'Jam/Scenes & Scripts/Game Scenes/button_app_logo.gd
@@ -8,19 +8,21 @@ var btn_tween : Tween
 # Handles line boil and tweening
 func _on_btn_entered() -> void:
 	reset_btn_tween()
-	btn_tween.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_ELASTIC)
-	btn_tween.tween_property(self, "scale", Vector2(1.1, 1.1), 0.4)
+	#btn_tween.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_ELASTIC)
+	#btn_tween.tween_property(self, "scale", Vector2(1.1, 1.1), 0.4)
 	
-	phone_line_boiler.get_animation('bg_line_boil').track_set_enabled(1, false)
-	button_boiler.play('logo_boiler')
+	#phone_line_boiler.get_animation('bg_line_boil').track_set_enabled(1, false)
+	#button_boiler.play('logo_boiler')
+	pass
 
 func _on_btn_exited() -> void:
-	reset_btn_tween()
-	btn_tween.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_ELASTIC)
-	btn_tween.tween_property(self, "scale", Vector2(1, 1), 0.4)
+	#reset_btn_tween()
+	#btn_tween.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_ELASTIC)
+	#btn_tween.tween_property(self, "scale", Vector2(1, 1), 0.4)
 	
-	phone_line_boiler.get_animation('bg_line_boil').track_set_enabled(1, true)
-	button_boiler.stop()
+	#phone_line_boiler.get_animation('bg_line_boil').track_set_enabled(1, true)
+	#button_boiler.stop()
+	pass
 	
 func reset_btn_tween() -> void:
 	if btn_tween:
