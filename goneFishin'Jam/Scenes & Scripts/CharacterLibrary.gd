cc09cdfbf76d09b6d658269e04903b759c789833
indecisivemagician <163153852+Indecisivv@users.noreply.github.com>
2026-08-24 06:46:35 -0400

    bug fixes
    FIXED
    - various typos
    -character name mispelling
    -translation fixed for khanh date cg
    -during ada's timeline, the text of the most intense choice is displayed as dialogue. the last line is also displayed twice.
    -when failing a date, the "[name] has snapped the line..." text is displayed erroneously.
    
    Known issues:
    ending text fading before ending cg

--- goneFishin'Jam/Scenes & Scripts/CharacterLibrary.gd
+++ goneFishin'Jam/Scenes & Scripts/CharacterLibrary.gd
@@ -1,8 +1,7 @@
 extends Node
 
-var ada     : CharacterClass = CharacterClass.new("Ada", "28", tr("CHAR_ADA_PRONOUNS"),tr("CHAR_ADA_DESC"))
+var ada     : CharacterClass = CharacterClass.new("Ada", "28", tr("CHAR_ADA_PRONOUNS"), tr("CHAR_ADA_DESC"))
 
-var khanh   : CharacterClass = CharacterClass.new("Khanh", "25", tr("CHAR_KHANH_PRONOUNS"),tr("CHAR_KHANH_DESC"))
+var khanh   : CharacterClass = CharacterClass.new("Khanh", "25", tr("CHAR_KHANH_PRONOUNS"), tr("CHAR_KHANH_DESC"))
 
-var soccoro : CharacterClass = CharacterClass.new("Soccoro", "29", tr("CHAR_SOCORRO_PRONOUNS"),tr("CHAR_SOCORRO_DESC"))
- 
+var soccoro : CharacterClass = CharacterClass.new("Soccoro", "29", tr("CHAR_SOCORRO_PRONOUNS"), tr("CHAR_SOCORRO_DESC"))
--- goneFishinJam/Scenes & Scripts/CharacterLibrary.gd
+++ goneFishinJam/Scenes & Scripts/CharacterLibrary.gd
@@ -4,5 +4,5 @@ var ada     : CharacterClass = CharacterClass.new("Ada", "28", tr("CHAR_ADA_PRON
 
 var khanh   : CharacterClass = CharacterClass.new("Khanh", "25", tr("CHAR_KHANH_PRONOUNS"),tr("CHAR_KHANH_DESC"))
 
-var soccoro : CharacterClass = CharacterClass.new("Soccoro", "29", tr("CHAR_SOCORRO_PRONOUNS"),tr("CHAR_SOCORRO_DESC"))
+var soccoro : CharacterClass = CharacterClass.new("Socorro", "29", tr("CHAR_SOCORRO_PRONOUNS"),tr("CHAR_SOCORRO_DESC"))
  
